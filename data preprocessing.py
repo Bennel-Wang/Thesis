2417cb8c051c56d93e3923a7aa6ef94bb0bd3258
Bennel-Wang <jinwang_96@163.com>
2020-12-08 04:29:19 +0100

    Attack tree grouping

--- data preprocessing.py
+++ data preprocessing.py
@@ -15,6 +15,8 @@ if __name__ == '__main__':
     with open('/home/jin/Documents/DARPA2000-LLS_DDOS_2.0.2/inside (test).csv', 'r') as f:
         reader = csv.reader(f)
         result = []
+        group = 0
+        IpGroup = collections.defaultdict(int)
         SrcIpDict = collections.defaultdict(int)
         DesIpDict = collections.defaultdict(int)
         relatedIpDict = collections.defaultdict(int)
@@ -35,32 +37,33 @@ if __name__ == '__main__':
                     l = {'No.id': l[0], 'Time': l[1], 'SrcIp': l[2], 'DesIp': l[3], 'Protocol': l[4], 'SrcPort':'-','DesPort':'-', 'Len': l[5],
                          'Info': l[6]}
 
+
                 #add the source Ips to the frequency dictionary, build relationship between the previous dest and the current src
-                if (SrcIpDict[l['SrcIp']] == 0):
-                    SrcIpDict[l['SrcIp']] = 1
-                    if (DesIpDict[l['SrcIp']] != 0):
+                if (SrcIpDict[l['SrcIp']] == 0):                    #SrcIp not as SrcIp before
+                    SrcIpDict[l['SrcIp']] = 1                       #initialize the frequency of SrcIP
+                    if (DesIpDict[l['SrcIp']] == 0):                #and SrcIp not as DesIp before
+                        group = group + 1                           #new a group and add the SrcIP and DesIp to the group
+                        IpGroup[l['SrcIp']] = group
+                        IpGroup[l['DesIp']] = IpGroup[l['SrcIp']]
+                        print('Initialize SrcIp ', l['SrcIp'], ' > ', l['DesIp'], ' time = ', l['Time'],  l['Info'], 'Attack tree group = ', group)
+                    else:                                           #but Src as Des before, link the path
                         relatedIpDict[l['SrcIp']] = relatedIpDict[l['SrcIp']] + 1
-                        print(l['SrcIp'], ' > ', l['DesIp'] )
-                    else:
-                        print('Initialize SrcIp ', l['SrcIp'], ' > ', l['DesIp'])
-                else:
-                    SrcIpDict[l['SrcIp']] = SrcIpDict[l['SrcIp']] + 1
-                    #relatedIpDict[l['SrcIp']] = relatedIpDict[l['SrcIp']] + 1
-                    print(l['SrcIp'], ' > ', l['DesIp'] )
+                        IpGroup[l['DesIp']] = IpGroup[l['SrcIp']]
+                        print(l['SrcIp'], ' > ', l['DesIp'],  ' time = ', l['Time'], l['Info'], ' group = ', IpGroup[l['SrcIp']])
+                else:                                               #SrcIp as Src before
+                    SrcIpDict[l['SrcIp']]= SrcIpDict[l['SrcIp']] + 1
+                    IpGroup[l['DesIp']] = IpGroup[l['SrcIp']]
+                    print(l['SrcIp'], ' > ', l['DesIp'], ' time = ', l['Time'], l['Info'], ' group = ', IpGroup[l['SrcIp']])
 
                 #add the destioation Ips to the frequency dictionary,  the previous des and the current des
                 if (DesIpDict[l['DesIp']] == 0):
                     DesIpDict[l['DesIp']] = 1
-                    #if (SrcIpDict[l['DesIp']] != 0):
-                    #    relatedIpDict[l['DesIp']] = relatedIpDict[l['DesIp']] + 1
                 else:
                     DesIpDict[l['DesIp']] = DesIpDict[l['DesIp']] + 1
                     relatedIpDict[l['DesIp']] = relatedIpDict[l['DesIp']] + 1
-                    #print(l)
-                    #print('DesIp:', l['DesIp'])
 
         for relatedIp in relatedIpDict:
-            print('relatedIp = ',relatedIp, 'relatedFreq = ',relatedIpDict[relatedIp])
+            print('relatedIp = ', relatedIp, 'relatedFreq = ', relatedIpDict[relatedIp])
 
             #result.append(l)
         #print(result)
