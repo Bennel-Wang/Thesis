27619a7aed1f3570c0eb3f060f4714381c16a3e7
Bennel-Wang <jinwang_96@163.com>
2020-12-09 10:33:28 +0100

    time slide window but without redundancy removal

--- data preparation2.py
+++ data preparation2.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 #whether the gap of two inputs is bigger the the preset value
 def validTimeGap(timeFormer, timeLatter, validGap):
-    if timeLatter - timeFormer > validGap:
+    if float(timeLatter) - float(timeFormer) > validGap:
         return False
     else:
         return True
@@ -13,14 +13,16 @@ def validTimeGap(timeFormer, timeLatter, validGap):
 if __name__ == '__main__':
 
     #initialization
-    with open('/home/jin/Documents/DARPA2000-LLS_DDOS_2.0.2/inside.csv', 'r') as f:
+    with open('/home/jin/Documents/DARPA2000-LLS_DDOS_2.0.2/inside (test).csv', 'r') as f:
         reader = csv.reader(f)
         result = [['Ip', 'Time', 'Type', 'attack tree group']]
         group = 0
+        validInterval = 0.1
         IpGroup = collections.defaultdict(int)
-        SrcIpDict = collections.defaultdict(int)
-        DesIpDict = collections.defaultdict(int)
-        relatedIpDict = collections.defaultdict(int)
+        IpLastTime = collections.defaultdict(str)
+        SrcIpFreq = collections.defaultdict(int)
+        DesIpFreq = collections.defaultdict(int)
+        #relatedIpDict = collections.defaultdict(int)
         for (i,l) in enumerate(reader):
 
             # remove the head
@@ -40,9 +42,11 @@ if __name__ == '__main__':
 
 
                 #add the source Ips to the frequency dictionary, build relationship between the previous dest and the current src
-                if (SrcIpDict[l['SrcIp']] == 0):                    #SrcIp not as SrcIp before
-                    SrcIpDict[l['SrcIp']] = 1                       #initialize the frequency of SrcIP
-                    if (DesIpDict[l['SrcIp']] == 0):                #and SrcIp not as DesIp before
+                if (SrcIpFreq[l['SrcIp']] == 0):                    #SrcIp not as SrcIp before
+                    SrcIpFreq[l['SrcIp']] = 1                       #initialize the frequency of SrcIP
+                    if (DesIpFreq[l['SrcIp']] == 0):                #and SrcIp not as DesIp before
+                        IpLastTime[l['SrcIp']] = l['Time']
+                        IpLastTime[l['DesIp']] = l['Time']
                         group = group + 1                           #new a group and add the SrcIP and DesIp to the group
                         IpGroup[l['SrcIp']] = group
                         IpGroup[l['DesIp']] = IpGroup[l['SrcIp']]
@@ -50,26 +54,55 @@ if __name__ == '__main__':
                         #result.append([['Ip', 'Time', 'Type']])
                         result.append([])
                         result[IpGroup[l['SrcIp']]].append([l['SrcIp'] +' > '+ l['DesIp'], l['Time'], l['Info']])
-                    else:                                           #but Src as Des before, link the path
-                        relatedIpDict[l['SrcIp']] = relatedIpDict[l['SrcIp']] + 1
-                        IpGroup[l['DesIp']] = IpGroup[l['SrcIp']]
-                        print(l['SrcIp'], ' > ', l['DesIp'],  ' time = ', l['Time'], l['Info'], ' group = ', IpGroup[l['SrcIp']])
-                        result[IpGroup[l['SrcIp']]].append([l['SrcIp'] +' > '+ l['DesIp'], l['Time'], l['Info'] ])
+                    else:                                           #but Src as Des before
+                        #relatedIpDict[l['SrcIp']] = relatedIpDict[l['SrcIp']] + 1
+                        if (validTimeGap(IpLastTime[l['SrcIp']], l['Time'], validInterval)):    #if the time interval between Ip current and last appearance, link the path
+                            IpLastTime[l['SrcIp']] = l['Time']
+                            IpLastTime[l['DesIp']] = l['Time']
+                            IpGroup[l['DesIp']] = IpGroup[l['SrcIp']]
+                            print(l['SrcIp'], ' > ', l['DesIp'],  ' time = ', l['Time'], l['Info'], ' group = ', IpGroup[l['SrcIp']])
+                            result[IpGroup[l['SrcIp']]].append([l['SrcIp'] +' > '+ l['DesIp'], l['Time'], l['Info'] ])
+                        else:                                                       #otherwise, break and create a new path
+                            IpLastTime[l['SrcIp']] = l['Time']
+                            IpLastTime[l['DesIp']] = l['Time']
+                            group = group + 1  # new a group and add the SrcIP and DesIp to the group
+                            IpGroup[l['SrcIp']] = group
+                            IpGroup[l['DesIp']] = IpGroup[l['SrcIp']]
+                            print('Initialize SrcIp ', l['SrcIp'], ' > ', l['DesIp'], ' time = ', l['Time'], l['Info'],
+                                  'Attack tree group = ', IpGroup[l['SrcIp']])
+                            # result.append([['Ip', 'Time', 'Type']])
+                            result.append([])
+                            result[IpGroup[l['SrcIp']]].append([l['SrcIp'] + ' > ' + l['DesIp'], l['Time'], l['Info']])
                 else:                                               #SrcIp as Src before
-                    SrcIpDict[l['SrcIp']]= SrcIpDict[l['SrcIp']] + 1
-                    IpGroup[l['DesIp']] = IpGroup[l['SrcIp']]
-                    print(l['SrcIp'], ' > ', l['DesIp'], ' time = ', l['Time'], l['Info'], ' group = ', IpGroup[l['SrcIp']])
-                    result[IpGroup[l['SrcIp']]].append([l['SrcIp'] +' > '+ l['DesIp'], l['Time'], l['Info']])
+                    if (validTimeGap(IpLastTime[l['SrcIp']], l['Time'], validInterval)):   #if the time interval between Ip current and last appearance, link the path
+                        IpLastTime[l['SrcIp']] = l['Time']
+                        IpLastTime[l['DesIp']] = l['Time']
+                        SrcIpFreq[l['SrcIp']] = SrcIpFreq[l['SrcIp']] + 1
+                        IpGroup[l['DesIp']] = IpGroup[l['SrcIp']]
+                        print(l['SrcIp'], ' > ', l['DesIp'], ' time = ', l['Time'], l['Info'], ' group = ', IpGroup[l['SrcIp']])
+                        result[IpGroup[l['SrcIp']]].append([l['SrcIp'] +' > '+ l['DesIp'], l['Time'], l['Info']])
+                    else:                                                       # otherwise, break and create a new path
+                        IpLastTime[l['SrcIp']] = l['Time']
+                        IpLastTime[l['DesIp']] = l['Time']
+                        SrcIpFreq[l['SrcIp']] = SrcIpFreq[l['SrcIp']] + 1
+                        group = group + 1  # new a group and add the SrcIP and DesIp to the group
+                        IpGroup[l['SrcIp']] = group
+                        IpGroup[l['DesIp']] = IpGroup[l['SrcIp']]
+                        print('Initialize SrcIp ', l['SrcIp'], ' > ', l['DesIp'], ' time = ', l['Time'], l['Info'],
+                              'Attack tree group = ', IpGroup[l['SrcIp']])
+                        # result.append([['Ip', 'Time', 'Type']])
+                        result.append([])
+                        result[IpGroup[l['SrcIp']]].append([l['SrcIp'] + ' > ' + l['DesIp'], l['Time'], l['Info']])
 
                 #add the destioation Ips to the frequency dictionary,  the previous des and the current des
-                if (DesIpDict[l['DesIp']] == 0):
-                    DesIpDict[l['DesIp']] = 1
+                if (DesIpFreq[l['DesIp']] == 0):
+                    DesIpFreq[l['DesIp']] = 1
                 else:
-                    DesIpDict[l['DesIp']] = DesIpDict[l['DesIp']] + 1
-                    relatedIpDict[l['DesIp']] = relatedIpDict[l['DesIp']] + 1
+                    DesIpFreq[l['DesIp']] = DesIpFreq[l['DesIp']] + 1
+                    #relatedIpDict[l['DesIp']] = relatedIpDict[l['DesIp']] + 1
 
-        for relatedIp in relatedIpDict:
-            print('relatedIp = ', relatedIp, 'relatedFreq = ', relatedIpDict[relatedIp])
+        #for relatedIp in relatedIpDict:
+        #    print('relatedIp = ', relatedIp, 'relatedFreq = ', relatedIpDict[relatedIp])
 
         groupNum = len(result) - 1
         name = ['Ip', 'Time', 'Info']
